466fe9371f67b2aac7a5e3ef8d0ab029446dc845
Gerrit Nowald <gerrit.nowald@continental-corporation.com>
2022-03-28 10:32:31 +0200

    sorted functions

--- polygon.py
+++ polygon.py
@@ -28,7 +28,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # -------------------------------------------------------
-# functions
+# input check
 
 def _close_loop(vert):
     if vert.shape[0] < vert.shape[1]:
@@ -36,10 +36,33 @@ def _close_loop(vert):
     if not np.isclose(vert[-1,:], vert[0,:]).all():
         vert = np.append(vert,[vert[0,:]],axis=0)   # first = last vertice
     return vert
-   
-    
+
+# -------------------------------------------------------
+# edges
+
+def poly_L(vert):
+    # lengths of edges (Pythagorean theorem)
+    vert = _close_loop(vert)
+    return np.sqrt( np.sum( np.diff(vert, axis=0)**2, axis=1))
+
+def poly_CMvert(vert):
+    # centers of edges
+    vert = _close_loop(vert)
+    return ( vert[0:-1] + vert[1:] )/2
+
+def poly_angles(vert):
+    # inner angles
+    vert = _close_loop(vert)
+    vert = np.append([vert[-2,:]],vert,axis=0)  # second last in front of first vertice
+    vec = np.diff(vert, axis=0)                 # direction vectors of edges
+    L   = np.linalg.norm(vec, ord=2, axis=1)    # length of edges
+    return 180 - 180/np.pi*np.arccos( np.sum( vec[0:-1,:]*vec[1:,:], axis=1 ) / (L[0:-1]*L[1:]) )
+
+# -------------------------------------------------------
+# area
+
 def poly_A(vert, flagFM=False):
-    # Area (Gauss's area formula)
+    # area (Gauss's area formula)
     vert = _close_loop(vert)
     FM   = vert[0:-1,0] * vert[1:,1] - vert[1:,0] * vert[0:-1,1]
     A    = sum(FM)/2   # 0th moment of area
@@ -48,20 +71,12 @@ def poly_A(vert, flagFM=False):
     else:
         return A, FM
 
-
-def poly_CMvert(vert):
-    # Centers of edges
-    vert = _close_loop(vert)
-    return ( vert[0:-1] + vert[1:] )/2
-    
-
 def poly_CM(vert):
-    # Center of Mass
+    # center of mass
     CMvert = poly_CMvert(vert)
     A, FM  = poly_A(vert, flagFM=True)
     A1     = (FM @ CMvert)/3    # 1st moment of area
     return A1/A
-
 
 def poly_SMA(vert):
     # second moment of area wrt center of mass
@@ -72,12 +87,17 @@ def poly_SMA(vert):
     A2 = (FM @ B)/12 - CM**2*A    # 2nd moment of area
     return A2[::-1]
 
+# -------------------------------------------------------
+# solid of revolution
 
-def poly_L(vert):
-    # Lengths of edges (Pythagorean theorem)
-    vert = _close_loop(vert)
-    return np.sqrt( np.sum( np.diff(vert, axis=0)**2, axis=1))
-
+def poly_Arot(vert, axis=0):
+    # surface areas of solid of revolution (Pappus's centroid theorem)
+    L      = poly_L(vert)
+    CMvert = poly_CMvert(vert)
+    if axis == 0:
+        return L*2*np.pi*CMvert[:,1]  # revolution around x-axis
+    elif axis == 1:
+        return L*2*np.pi*CMvert[:,0]  # revolution around y-axis
 
 def poly_Vrot(vert, axis=0):
     # volume of solid of revolution (Pappus's centroid theorem)
@@ -88,25 +108,8 @@ def poly_Vrot(vert, axis=0):
     elif axis == 1:
         return A*2*np.pi*CM[0]  # revolution around y-axis
 
-
-def poly_Arot(vert, axis=0):
-    # Surface areas of solid of revolution (Pappus's centroid theorem)
-    L      = poly_L(vert)
-    CMvert = poly_CMvert(vert)
-    if axis == 0:
-        return L*2*np.pi*CMvert[:,1]  # revolution around x-axis
-    elif axis == 1:
-        return L*2*np.pi*CMvert[:,0]  # revolution around y-axis    
-
-
-def poly_angles(vert):
-    # inner angles
-    vert = _close_loop(vert)
-    vert = np.append([vert[-2,:]],vert,axis=0)  # second last in front of first vertice
-    vec = np.diff(vert, axis=0)                 # direction vectors of edges
-    L   = np.linalg.norm(vec, ord=2, axis=1)    # length of edges
-    return 180 - 180/np.pi*np.arccos( np.sum( vec[0:-1,:]*vec[1:,:], axis=1 ) / (L[0:-1]*L[1:]) )
-
+# -------------------------------------------------------
+#  plot
 
 def poly_plot(vert):
     # plot Polygon
