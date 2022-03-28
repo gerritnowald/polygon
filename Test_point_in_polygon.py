58018a5ee5f7f16ca7c2ddf51dd63d3703fca832
Gerrit Nowald <gerrit.nowald@gmail.com>
2022-03-28 20:39:21 +0200

    changed order of conditions

--- Test_point_in_polygon.py
+++ Test_point_in_polygon.py
@@ -9,24 +9,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import polygon as pg
 
+import time
+
 # -------------------------------------------------------
 # Input
 
-Point = [4, 1]
+N = 1000
+
 
 
 # rectangle
-b = 5
-h = 2
-vert = np.array([
-    [0, 0],
-    [b, 0],
-    [b, h],
-    [0, h],
-    ])
-
+# b = 5
+# h = 2
+# vert = np.array([
+#     [0, 0],
+#     [b, 0],
+#     [b, h],
+#     [0, h],
+#     ])
 
 # triangle
+# Points = np.hstack(( np.random.rand(N,1)*5-1, np.random.rand(N,1)*10-1.5 ))
 # a = 3
 # h = 7
 # vert = np.array([
@@ -34,6 +37,21 @@ vert = np.array([
 #     [a, 0],
 #     [a/2, h],
 #     ])
+
+# heart
+Points = np.hstack(( np.random.rand(N,1)*6-3, np.random.rand(N,1)*10-2 ))
+vert = np.array([
+    [0, 0],
+    [1.75,4],
+    [1.5,6],
+    [1,7],
+    [0.25,6],
+    [0,5],
+    [-0.25,6],
+    [-1,7],
+    [-1.5,6],
+    [-1.75,4],
+    ])
 
 # -------------------------------------------------------
 # Function
@@ -53,19 +71,27 @@ def point_in_polygon(vert, point=[0,0]):
     for j in range(vert.shape[0]-1):    # for each edge check if the line crosses
         i = j + 1                       # next vertice
         if vert[j,1] != vert[i,1]:      # edge not parallel to x-axis (singularity)
-            Qx = (vert[j,0]-vert[i,0])*(point[1]-vert[i,1])/(vert[j,1]-vert[i,1]) + vert[i,0]   # x-coordinate of intersection
-            left    = point[0] < Qx     # point left of edge
-            between = (vert[i,1] > point[1]) != (vert[j,1] > point[1])  # one vertice needs to be above, one below the y coordinate
-            if left and between:        # line crosses edge
-                odd = not odd
+            # point between y-coordinates of edge
+            if (vert[i,1] > point[1]) != (vert[j,1] > point[1]):
+                # x-coordinate of intersection
+                Qx = (vert[j,0]-vert[i,0])*(point[1]-vert[i,1])/(vert[j,1]-vert[i,1]) + vert[i,0]
+                if point[0] < Qx:       # point left of edge
+                    odd = not odd       # line crosses edge
     return odd  # point is in polygon (not on the edge) if odd=true
 
 # -------------------------------------------------------
 # Test
 
 pg.poly_plot(vert)
-plt.plot(Point[0],Point[1],"+")
 
 vert = pg._close_loop(vert)
 
-print(f'Point in polygon: {point_in_polygon(vert, Point)}')
+start_time = time.time()
+
+for Point in Points:
+    if point_in_polygon(vert, Point):
+        plt.plot(Point[0],Point[1],"r+")
+    else:
+        plt.plot(Point[0],Point[1],"b+")
+        
+print(f"elapsed time: {time.time() - start_time} s")
