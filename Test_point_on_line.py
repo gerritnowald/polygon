25f7d3b340b018d55855c591a0ed906442bfea83
Gerrit Nowald <gerrit.nowald@continental-corporation.com>
2022-03-29 09:55:23 +0200

    switched from line to edges

--- Test_point_on_line.py
+++ Test_point_on_line.py
@@ -8,39 +8,51 @@ Created on Mon Mar 28 15:11:12 2022
 import numpy as np
 import matplotlib.pyplot as plt
 
+from polygon import *
+
 # -------------------------------------------------------
 # input
 
-P1 = np.array([2,1])
-P2 = np.array([4,5])
+# rectangle
+b = 5
+h = 2
+vert = np.array([
+    [0, 0],
+    [b, 0],
+    [b, h],
+    [0, h],
+    [0, 0],
+    ])
 
-Q = np.array([3,3])
+point = np.array([5,2])
 
 # -------------------------------------------------------
 # function
 
-def isPointOnLine(P1, P2, Q):
-    # Is point Q=[x3,y3] on line from P1=[x1,y1] to P2=[x2,y2]
+def isPointOnEdge(vert, point):
+    # computes the distance of a point from each edge. The point is on an edge,
+    # if the point is between the vertices and the distance is smaller than the rounding error.
     # https://de.mathworks.com/matlabcentral/answers/351581-points-lying-within-line
-    PQ  =  Q - P1   # Line from P1 to Q
-    P12 = P2 - P1   # Line from P1 to P2
-    L12 = np.sqrt(np.dot(P12,P12))
-    N   = P12/L12   # Normal along the line
-    
-    Dist  = abs(np.cross(N,PQ))     # Norm of distance vector
-    Limit = np.spacing(np.max(np.abs([P1, P2, Q])))*10   # Consider rounding errors
-    on    = Dist < Limit
-    if on:
-        L = np.dot(PQ,N)            # Projection of the vector from P1 to Q on the line:
-        on = (L>=0.0 and L<=L12)    # Consider end points  
+    for i in range(vert.shape[0]-1):# for each edge
+        j     = i + 1
+        PQ    =    point - vert[i,]       # Line from P1 to Q
+        P12   = vert[j,] - vert[i,]       # Line from P1 to P2
+        L12   = np.sqrt(np.dot(P12,P12))  # length of P12
+        N     = P12/L12                   # Normal along P12
+        Dist  = abs(np.cross(N,PQ))     # Norm of distance vector
+        Limit = np.spacing(np.max(np.abs([vert[i,], vert[j,], point])))*10   # Consider rounding errors
+        on    = Dist < Limit
+        if on:
+            L = np.dot(PQ,N)            # Projection of the vector from P1 to Q on the line:
+            on = (L>=0.0 and L<=L12)    # Consider end points  
+            return on
     return on
 
 # -------------------------------------------------------
 # test
 
-plt.plot(P1[0],P1[1],"b+")
-plt.plot(P2[0],P2[1],"b+")
-plt.plot([P1[0],P2[0]],[P1[1],P2[1]],'k')
-plt.plot(Q[0],Q[1],"r+")
+poly_plot(vert)
 
-print(f'Point on line: {isPointOnLine(P1, P2, Q)}')
+plt.plot(point[0],point[1],"r+")
+
+print(f'Point on edge: {isPointOnEdge(vert, point)}')
