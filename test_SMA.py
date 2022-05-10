ec72b93aded11528f69fc34bc7942f018b1a14c4
Gerrit Nowald <gerrit.nowald@continental-corporation.com>
2022-05-10 11:03:46 +0200

    cleanup of test

--- test_SMA.py
+++ test_SMA.py
@@ -10,15 +10,46 @@ import matplotlib.pyplot as plt
 
 from polygon import polygon
 
+# -----------------------------------------------------------------------------
+# examples
 
-L = 5
-t = 1
+# rectangle
+# L = 5
+# t = 1
+# Vertices = [[0,0],[0,L],[t,L],[t,0]]
+# Ixx_analytic = [L**3*t/12, L*t**3/12]
+# Ixy_analytic = 0
 
 
-# Vertices = [[0,0],[0,L],[t,L],[t,0]]    # rectangle
-Vertices = [[0,0],[0,L],[t,L],[t,t],[L,t],[L,0]]  # L shape
+# L-shape symmetric
+# L = 5
+# t = 1
+# Vertices = [[0,0],[0,L],[t,L],[t,t],[L,t],[L,0]]
+# Ixx_analytic = t*(5*L**2-5*L*t+t**2)*(L**2-L*t+t**2)/12/(2*L-t)
+# Ixy_analytic = L**2*t*(L-t)**2/4/(t-2*L)
+
+
+# L-shape
+a  = 4
+b1 = 2
+b2 = 3
+c  = 1
+Vertices = [[0,0],[0,a],[b1+b2,a],[b1+b2,a-c],[b1,a-c],[b1,0]]
+A = a*b1 + b2*c
+Sx = (b1/2*a*b1 + (b1+b2/2)*b2*c)/A
+Sy = (a/2*a*b1 + (a-c/2)*b2*c)/A
+Ixx_analytic = [
+    a**3*b1/12 + (a/2-Sy)**2*a*b1 + c**3*b2/12 + (a-c/2-Sy)**2*b2*c ,
+    a*b1**3/12 + (b1/2-Sx)**2*a*b1 + c*b2**3/12 + (b1+b2/2-Sx)**2*b2*c
+    ]
+Ixy_analytic = - (b1/2-Sx)*(a/2-Sy)*a*b1 - (b1+b2/2-Sx)*(a-c/2-Sy)*b2*c
+
 
 P = polygon(Vertices)
+
+# -----------------------------------------------------------------------------
+# manipulation
+
 # P -= P.CenterMass
 # P *= [1,-1]
 
@@ -37,6 +68,8 @@ P2 = P
 # P2 = P*[1,-1]
 # P2 = P*[-1,-1]
 
+# -----------------------------------------------------------------------------
+# plot
 
 plt.figure()
 # P.plot()
@@ -45,6 +78,8 @@ plt.plot(P2.CenterMass[0],P2.CenterMass[1],"+")
 plt.axis('equal')
 plt.show()
 
+# -----------------------------------------------------------------------------
+# calculation
 
 vert = P2.Vertices
     
@@ -67,28 +102,17 @@ CenterMass = (FM @ EdgesMiddle)/3/AreaSigned
 # https://en.wikipedia.org/wiki/Second_moment_of_area
 Brr    = ri**2 + ri*rip1 + rip1**2
 Bxy    = xi*yip1 + 2*xi*yi + 2*xip1*yip1 + xip1*yi
-# IyyIxx = abs(FM @ Brr)/12 - Area*CenterMass**2  # correct
-IyyIxx =   abs( (FM @ Brr)/12 - AreaSigned*CenterMass**2 )  # correct
+IyyIxx =   abs( (FM @ Brr)/12 - AreaSigned*CenterMass**2 )
 Ixy    = - abs( (FM @ Bxy)/24 - AreaSigned*CenterMass[0]*CenterMass[1] )
 
 SecondMomentArea = np.hstack((IyyIxx[::-1], Ixy))
 
+# -----------------------------------------------------------------------------
+# comparison results
 
-Ixx_analytic = t*(5*L**2-5*L*t+t**2)*(L**2-L*t+t**2)/12/(2*L-t)
-
-Ixy_analytic = L**2*t*(L-t)**2/4/(t-2*L)
-
-Ixy_own = ( (t+(L-t)/2-P.CenterMass[1])*(t/2-P.CenterMass[0])*t*(L-t) +
-           (t/2-P.CenterMass[1])*(L/2-P.CenterMass[0])*L*t )
-
-
-# print(Ixx_analytic)
-# print(L**3*t/12)
-# print(SecondMomentArea[0])
-# print(L*t**3/12)
-# print(SecondMomentArea[1])
-
+print(Ixx_analytic)
+print(SecondMomentArea[0])
+print(SecondMomentArea[1])
 
 print(Ixy_analytic)
-# print(Ixy_own)
 print(SecondMomentArea[2])
