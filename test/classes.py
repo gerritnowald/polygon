235205ab5505945139c77da0c9d6129cbb326e73
Gerrit Nowald <gerrit.nowald@gmail.com>
2022-12-21 19:29:24 +0100

    test removing polygonBase

--- test/classes.py
+++ test/classes.py
@@ -5,42 +5,40 @@ Created on Wed Dec 21 19:07:21 2022
 @author: Gerrit Nowald
 """
 
-class _polygonBase():
-    def __init__(self, vert, axis):
+class polygon():
+    def __new__(cls, vert, axis=None):
+        triangle = vert == 3
+        solidrev = axis is not None
+        if triangle and not solidrev:
+            return super().__new__(_triangle)
+        elif solidrev and not triangle:
+            return super().__new__(_solid)
+        elif solidrev and triangle:
+            return super().__new__(_triangle_solid)
+        else:
+            return super().__new__(cls)
+    def __init__(self, vert, axis=None):
         self.polygon_attribute = 1
     def polygon_method(self):
         print('polygon')
 
-class _triangle(_polygonBase):
-    def __init__(self, vert, axis):
+class _triangle(polygon):
+    def __init__(self, vert, axis=None):
         super().__init__(vert, axis)
         self.triangle_attribute = 2
     def triangle_method(self):
         print('triangle')
 
-class _solid(_polygonBase):
+class _solid(polygon):
     def __init__(self, vert, axis):
         super().__init__(vert, axis)
         self.solid_attribute = 3
     def solid_method(self):
         print('solid of revolution')
 
-class _triangle_solid(_triangle,_solid):
+class _triangle_solid(_triangle, _solid):
     def __init__(self, vert, axis):
         super().__init__(vert, axis)
-
-class polygon():
-    def __new__(self, vert, axis=None):
-        triangle = vert == 3
-        solidrev = axis is not None
-        if triangle and not solidrev:
-            return _triangle(vert, axis)
-        elif solidrev and not triangle:
-            return _solid(vert, axis)
-        elif solidrev and triangle:
-            return _triangle_solid(vert, axis)
-        else:
-            return _polygonBase(vert, axis)
 
 
 P = polygon(2)
