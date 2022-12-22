2d34d842c2f26de1ab459cd7b3fcd77bcbadedb9
Gerrit Nowald <91027665+gerritnowald@users.noreply.github.com>
2022-12-22 11:57:56 +0100

    moved back to old method

--- test/classes.py
+++ test/classes.py
@@ -1,47 +1,49 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Dec 21 19:07:21 2022
+https://stackoverflow.com/questions/74874119/python-overlapping-non-exclusive-inheritance-to-have-methods-available-based
 
 @author: Gerrit Nowald
 """
 
-class polygon():
-    def __new__(cls, vert, axis=None):
-        triangle = vert == 3
-        solidrev = axis is not None
-        if triangle and not solidrev:
-            return super().__new__(_triangle)
-        elif solidrev and not triangle:
-            return super().__new__(_solid)
-        elif solidrev and triangle:
-            return super().__new__(_triangle_solid)
-        else:
-            return super().__new__(cls)
-    def __init__(self, vert, axis=None):
+class _polygonBase():
+    def __init__(self, vert, axis):
         self.polygon_attribute = 1
     def polygon_method(self):
         print('polygon')
 
-class _triangle(polygon):
-    def __init__(self, vert, axis=None):
+class _triangle(_polygonBase):
+    def __init__(self, vert, axis):
         super().__init__(vert, axis)
         self.triangle_attribute = 2
     def triangle_method(self):
         print('triangle')
 
-class _solid(polygon):
+class _solid(_polygonBase):
     def __init__(self, vert, axis):
         super().__init__(vert, axis)
         self.solid_attribute = 3
     def solid_method(self):
         print('solid of revolution')
 
-class _triangle_solid(_triangle, _solid):
+class _solid_and_triangle(_triangle, _solid):
     def __init__(self, vert, axis):
         super().__init__(vert, axis)
 
+class polygon():
+    def __new__(self, vert, axis=None):
+        isTriangle = vert == 3
+        isSolidRev = axis is not None
+        if isTriangle and not isSolidRev:
+            return _triangle(vert, axis)
+        elif isSolidRev and not isTriangle:
+            return _solid(vert, axis)
+        elif isSolidRev and isTriangle:
+            return _solid_and_triangle(vert, axis)
+        else:
+            return _polygonBase(vert, axis)
 
-P = polygon(2)
+
+# P = polygon(2)
 # P = polygon(2,axis=0)
 # P = polygon(3)
-# P = polygon(3,axis=0)
+P = polygon(3,axis=0)
